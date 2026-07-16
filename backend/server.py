25799a73f52b8851b4e0fc34e7b1e8e42e627358
vpdsurveillance <vpdsurveillancesphcl@gmail.com>
2026-07-16 23:07:17 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -790,8 +790,10 @@ app.include_router(api)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://stock-and-issue-tracker-frontend.onrender.com"
+    ],
     allow_credentials=True,
-    allow_origins=["https://stock-and-issue-tracker-frontend.onrender.com" ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
