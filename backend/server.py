0499dbe4a46167206e22aceec3d37b3f0e4d5627
vpdsurveillance <vpdsurveillancesphcl@gmail.com>
2026-07-16 22:57:25 +0530

    Update CORS origins to specific frontend URL

--- backend/server.py
+++ backend/server.py
@@ -791,7 +791,7 @@ app.include_router(api)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://stock-and-issue-tracker-frontend.onrender.com" ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
